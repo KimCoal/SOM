40b5b6122ac77fab1a06ef7cabcbdfb886d322b7
KimCoal <johnlove2572@gmail.com>
2023-05-12 14:17:50 +0900

    UserResponse Dto

--- FRONT/som/src/App.tsx
+++ FRONT/som/src/App.tsx
@@ -1,6 +1,42 @@
 import './App.css';
 
+import { useCookies } from 'react-cookie';
+import { useUserStore } from './stores';
+import { useLocation } from 'react-router-dom';
+import axios, { AxiosResponse } from 'axios';
+import { GET_USER_URL, authorizationHeader } from './constants/api';
+import ResponseDto from './apis/response';
+
 function App() {
+
+  const path = useLocation();
+  const { setUser } = useUserStore();
+  const [ cookies ] = useCookies();
+
+  const getUser = (accessToken: string) => {
+    axios.get(GET_USER_URL, authorizationHeader(accessToken))
+      .then((response) => getUserResponseHandler(response))
+      .catch((error) => getUserErrorHandler(error));
+  }
+
+  const getUserResponseHandler = (response: AxiosResponse<any, any>) => {
+    const { result, message, data } = response.data as ResponseDto<any>;
+    if (!result || !data) {
+      return;
+    }
+    const user = data as GetUserResponseDto;
+    setUser(user);
+  }
+
+  const getUserErrorHandler = (error: any) => {
+    console.log(error.message);
+  }
+
+  useEffect(() => {
+    const accessToken = cookies.accessToken;
+    if (accessToken) getUser(accessToken);
+  }, [path]);
+
   return (
     <div className="App">
       <header className="App-header">
